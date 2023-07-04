cdaa336d4ccca322b6c51dba01e22e766427a1c1
1frstn <krmilhn@icloud.com>
2023-07-04 10:52:55 +1000

    new feature added

--- 3-Playing-with-JS-Variable/main.js
+++ 3-Playing-with-JS-Variable/main.js
@@ -2,7 +2,11 @@ const inputs = document.querySelectorAll('.controls input');
 
     function handleUpdate() {
       const suffix = this.dataset.sizing || '';
+      console.log("SUFFIC>>>>>",suffix);
+      console.log("THISS>>>",this);
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+      console.log(document.documentElement);
+
     }
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
